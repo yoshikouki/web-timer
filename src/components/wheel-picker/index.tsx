ed1a01b21891e095318f471b779e547fbfebc6e1
yoshikouki <yoshikouki@pepabo.com>
2024-11-10 15:37:40 +0900

    Make touch action enabled to WheelPicker

--- src/components/wheel-picker/index.tsx
+++ src/components/wheel-picker/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { useCallback, useEffect, useRef } from "react";
+import { type TouchEventHandler, useCallback, useEffect, useRef } from "react";
 
 export const WheelPicker = ({
   children,
@@ -21,6 +21,7 @@ export const WheelPicker = ({
   const containerRef = useRef<HTMLDivElement | null>(null);
   const scrollDelta = useRef(0);
   const valueIndex = useRef(options.findIndex((v) => v === value) ?? 0);
+  const touchStartY = useRef<number | null>(null);
 
   const onWheel = useCallback(
     (event: WheelEvent) => {
@@ -41,17 +42,53 @@ export const WheelPicker = ({
     [options, onChange, scrollThreshold],
   );
 
+  const onTouchStart: TouchEventHandler = (event) => {
+    touchStartY.current = event.touches[0].clientY;
+  };
+
+  const onTouchMove = (event: TouchEvent) => {
+    if (touchStartY.current === null) return;
+    event.preventDefault();
+    const currentY = event.touches[0].clientY;
+    const deltaY = touchStartY.current - currentY;
+    scrollDelta.current += -deltaY;
+    touchStartY.current = currentY;
+    if (Math.abs(scrollDelta.current) < scrollThreshold) return;
+    const steps = Math.sign(scrollDelta.current);
+    scrollDelta.current = 0;
+    const nextIndex = valueIndex.current - steps;
+    const nextValue = options[nextIndex];
+    if (nextValue === undefined) return;
+    onChange(nextValue);
+    valueIndex.current = nextIndex;
+  };
+
+  const onTouchEnd = () => {
+    touchStartY.current = null;
+  };
+
   useEffect(() => {
-    containerRef.current?.addEventListener("wheel", onWheel, {
-      passive: false,
-    });
+    const container = containerRef.current;
+    if (!container) return;
+
+    // passive: false to enable event.preventDefault
+    container.addEventListener("wheel", onWheel, { passive: false });
+    container.addEventListener("touchmove", onTouchMove, { passive: false });
+
     return () => {
-      containerRef.current?.removeEventListener("wheel", onWheel);
+      container.removeEventListener("wheel", onWheel);
+      container.removeEventListener("touchmove", onTouchMove);
     };
-  }, [onWheel]);
+    // biome-ignore lint/correctness/useExhaustiveDependencies: React Compiler is enabled
+  }, [onWheel, onTouchMove]);
 
   return (
-    <div ref={containerRef} className={cn("touch-none select-none", className)}>
+    <div
+      ref={containerRef}
+      className={cn("touch-none select-none", className)}
+      onTouchStart={onTouchStart}
+      onTouchEnd={onTouchEnd}
+    >
       {children}
     </div>
   );
