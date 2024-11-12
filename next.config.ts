8b5b26fd754a4c816722a4bc5dba087259ac188f
yoshikouki <yoshikouki@pepabo.com>
2024-11-12 23:35:24 +0900

    Disable Next.js indicators

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: {
+    appIsrStatus: false,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
+  },
   experimental: {
     reactCompiler: true,
   },
