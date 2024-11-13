724799e8cf1cc34887453f207e8dff31a9f94e86
yoshikouki <yoshikouki@pepabo.com>
2024-11-13 23:33:02 +0900

    Refine FontSelector style

--- src/components/timer/timer-controller-settings/font-selector.tsx
+++ src/components/timer/timer-controller-settings/font-selector.tsx
@@ -20,14 +20,14 @@ export const FontSelector = () => {
         update({ font: value });
       }}
     >
-      <SelectTrigger className="h-full w-full">
+      <SelectTrigger className="h-full w-full py-[2px] pl-0">
         <SelectValue placeholder="Font" />
       </SelectTrigger>
       <SelectContent className="w-full">
-        <SelectItem value="poppins">
+        <SelectItem value="poppins" className="py-[2px] pl-0">
           <div
             className={cn(
-              "grid grid-cols-[90px_1fr] p-4 tabular-nums",
+              "grid grid-cols-[90px_1fr] px-4 py-2 tabular-nums",
               fonts.poppins.className,
             )}
           >
@@ -35,10 +35,10 @@ export const FontSelector = () => {
             <span className="text-left">Poppins</span>
           </div>
         </SelectItem>
-        <SelectItem value="sora">
+        <SelectItem value="sora" className="py-[2px] pl-0">
           <div
             className={cn(
-              "grid grid-cols-[90px_1fr] p-4 tabular-nums",
+              "grid grid-cols-[90px_1fr] px-4 py-2 tabular-nums",
               fonts.sora.className,
             )}
           >
@@ -46,10 +46,10 @@ export const FontSelector = () => {
             <span className="text-left">Sora</span>
           </div>
         </SelectItem>
-        <SelectItem value="teko">
+        <SelectItem value="teko" className="py-[2px] pl-0">
           <div
             className={cn(
-              "grid grid-cols-[90px_1fr] p-4 tabular-nums",
+              "grid grid-cols-[90px_1fr] px-4 py-2 tabular-nums",
               fonts.teko.className,
             )}
           >
@@ -57,10 +57,10 @@ export const FontSelector = () => {
             <span className="text-left">Teko</span>
           </div>
         </SelectItem>
-        <SelectItem value="comfortaa">
+        <SelectItem value="comfortaa" className="py-[2px] pl-0">
           <div
             className={cn(
-              "grid grid-cols-[90px_1fr] p-4 tabular-nums",
+              "grid grid-cols-[90px_1fr] px-4 py-2 tabular-nums",
               fonts.comfortaa.className,
             )}
           >
