84e0fa7f39ee59aefef53c74070065aa4fd51331
yoshikouki <yoshikouki@pepabo.com>
2024-11-17 09:58:11 +0900

    Sort settings item

--- src/components/timer/timer-controller-settings/index.tsx
+++ src/components/timer/timer-controller-settings/index.tsx
@@ -25,11 +25,46 @@ export const TimerControllerSettings = ({
   return (
     <Popover>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
-      <PopoverContent className="w-80" align="end">
-        <div className="grid gap-5">
+      <PopoverContent className="w-80 bg-popover/90" align="end">
+        <div className="grid gap-10">
           <div className="py-2">
             <h3 className="font-medium leading-none">Settings</h3>
           </div>
+
+          <div className="grid gap-2">
+            <div className="flex items-center justify-between gap-1">
+              <h4 className="flex items-center gap-1 font-medium">
+                <BellIcon className="h-4 w-4" />
+                Finish sound
+              </h4>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="flex h-10 items-center justify-center [&_svg]:size-4"
+                onMouseDown={() => playFinishSound()}
+              >
+                <Volume2Icon className="h-4 w-4" />
+              </Button>
+            </div>
+            <FinishSoundSelector />
+          </div>
+
+          <div className="grid gap-2">
+            <div className="flex items-center justify-between gap-1">
+              <h4 className="flex items-center gap-1 font-medium">
+                {timerControlSettings.finishSoundVolume === 0 ? (
+                  <VolumeIcon className="h-4 w-4" />
+                ) : timerControlSettings.finishSoundVolume < 0.5 ? (
+                  <Volume1Icon className="h-4 w-4" />
+                ) : (
+                  <Volume2Icon className="h-4 w-4" />
+                )}
+                Finish sound volume
+              </h4>
+            </div>
+            <FinishSoundVolumeSlider />
+          </div>
+
           <div className="grid gap-5">
             <div className="grid gap-2">
               <h4
@@ -41,40 +76,6 @@ export const TimerControllerSettings = ({
                 Font
               </h4>
               <FontSelector />
-            </div>
-
-            <div className="grid gap-2">
-              <div className="flex items-center justify-between gap-1">
-                <h4 className="flex items-center gap-1 font-medium">
-                  <BellIcon className="h-4 w-4" />
-                  Finish sound
-                </h4>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="flex h-10 items-center justify-center [&_svg]:size-4"
-                  onMouseDown={() => playFinishSound()}
-                >
-                  <Volume2Icon className="h-4 w-4" />
-                </Button>
-              </div>
-              <FinishSoundSelector />
-            </div>
-
-            <div className="grid gap-2">
-              <div className="flex items-center justify-between gap-1">
-                <h4 className="flex items-center gap-1 font-medium">
-                  {timerControlSettings.finishSoundVolume === 0 ? (
-                    <VolumeIcon className="h-4 w-4" />
-                  ) : timerControlSettings.finishSoundVolume < 0.5 ? (
-                    <Volume1Icon className="h-4 w-4" />
-                  ) : (
-                    <Volume2Icon className="h-4 w-4" />
-                  )}
-                  Finish sound volume
-                </h4>
-              </div>
-              <FinishSoundVolumeSlider />
             </div>
           </div>
         </div>
