76a6480ea49247b1608242f59f016011ebf6c0a4
yoshikouki <yoshikouki@gmail.com>
2024-12-06 00:04:53 +0900

    Animated button

--- src/components/timer/timer-controller-settings/horizontal-layout-toggle.tsx
+++ src/components/timer/timer-controller-settings/horizontal-layout-toggle.tsx
@@ -1,23 +1,33 @@
+import { Button } from "@/components/ui/button";
 import { Toggle } from "@/components/ui/toggle";
+import { cn } from "@/lib/utils";
 import { RotateCwSquareIcon } from "lucide-react";
 import { useTimer } from "../use-timer";
 
 export const HorizontalLayoutToggle = () => {
   const { timerControlSettings, updateTimerControlSettings } = useTimer();
+  const isHorizontal = timerControlSettings.orientation === "horizontal";
   return (
     <Toggle
       variant="outline"
       aria-label="Horizontal layout"
-      pressed={timerControlSettings.orientation === "horizontal"}
+      pressed={isHorizontal}
       onPressedChange={(checked) => {
         updateTimerControlSettings({
           orientation: checked ? "horizontal" : "vertical",
         });
       }}
-      className="h-10 w-fit px-4"
+      asChild
     >
-      <RotateCwSquareIcon className="size-6" />
-      Horizontal
+      <Button variant="outline" className="h-10 w-fit px-4">
+        <RotateCwSquareIcon
+          className={cn(
+            "size-6 transition-all duration-300 ease-in-out",
+            isHorizontal && "rotate-90",
+          )}
+        />
+        Horizontal
+      </Button>
     </Toggle>
   );
 };
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,11 +3,12 @@ import { type VariantProps, cva } from "class-variance-authority";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import styles from "./button-animation.module.css";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors" +
-    "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" +
-    "[&_svg]:pointer-events-none [&_svg]:size-6 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors " +
+    "focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 " +
+    "[&_svg]:pointer-events-none [&_svg]:size-6 [&_svg]:shrink-0 ",
   {
     variants: {
       variant: {
@@ -47,7 +48,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(
+          buttonVariants({ variant, size, className }),
+          styles.touchyButton,
+        )}
         ref={ref}
         {...props}
       />
